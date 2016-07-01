8101845e90ff1fe6f1611c832acd53d6eb91e1c2
TobiasEns <tobias.ens@docuware.com>
2016-07-01 17:50:54 +0200

    added connect to platform with specific timeout value

---
+++ Platform.NETClient-Async-Samples/Platform.NETClient-Async-Samples/PlatformClient.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DocuWare.Platform.ServerClient;
+using DocuWare.Services.Http.Client;
+
+namespace Platform.NETClient_Async_Samples
+{
+    class PlatformClient
+    {
+        public ServiceConnection Conn { get; set; }
+        public Uri BaseAddress { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlatformClient"/> class.
+        /// </summary>
+        /// <param name="hostAddress">The host address.</param>
+        /// <param name="user">The user.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="organization">The organization.</param>
+        public PlatformClient(Uri hostAddress, string user, string password, string organization = null)
+        {
+            BaseAddress = new Uri($"{hostAddress}/docuware/platform");
+            Conn = ServiceConnection.CreateAsync(BaseAddress, user, password, organization: organization).Result;
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlatformClient"/> class.
+        /// </summary>
+        /// <param name="hostAddress">The host address.</param>
+        /// <param name="user">The user.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="httpClientHandler">The HTTP client handler.</param>
+        /// <param name="organization">The organization.</param>
+        public PlatformClient(Uri hostAddress, string user, string password, HttpClientHandler httpClientHandler, string organization = null)
+        {
+            BaseAddress = new Uri($"{hostAddress}/docuware/platform");
+            Conn = ServiceConnection.CreateAsync(BaseAddress, user, password, organization: organization, httpClientHandler: httpClientHandler).Result;
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlatformClient"/> class.
+        /// </summary>
+        /// <param name="hostAddress">The host address.</param>
+        /// <param name="user">The user.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="timeoutValue">The timeout value.</param>
+        /// <param name="organization">The organization.</param>
+        public PlatformClient(Uri hostAddress, string user, string password, TimeSpan timeoutValue, string organization = null)
+        {
+            var httpClientHandler = CreateHttpClientHandler();
+            BaseAddress = new Uri($"{hostAddress}/docuware/platform");
+            Conn = ServiceConnection.CreateAsync(BaseAddress, user, password, organization: organization, httpClientHandler: httpClientHandler).Result;
+            SetProxyWithTimeout(timeoutValue, httpClientHandler);
+        }
+
+        /// <summary>
+        /// Sets the proxy with timeout.
+        /// If the WebRequestHandler has a timeout value, the lowest timeout value in HttpClient and WebRequestHandler will be used.
+        /// </summary>
+        /// <param name="timeSpan">The time span.</param>
+        /// <param name="httpClientHandler">The HTTP client handler.</param>
+        public void SetProxyWithTimeout(TimeSpan timeSpan, HttpClientHandler httpClientHandler)
+        {
+            var httpClient = new HttpClient(httpClientHandler)
+            {
+                BaseAddress = BaseAddress,
+                Timeout = timeSpan
+            };
+            // Set proxy in service description
+            Conn.ServiceDescription.SetProxy(httpClient);
+        }
+        /// <summary>
+        /// Creates the HTTP client handler.
+        /// </summary>
+        public static HttpClientHandler CreateHttpClientHandler()
+            => new HttpClientHandler { UseCookies = true };
+    }
+}
---
+++ Platform.NETClient-Async-Samples/Platform.NETClient-Async-Samples/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform.NETClient_Async_Samples
+{
+    /// <summary>
+    /// This samples depict actions by a single user, thus there is no need for an asynchronous authentication at the platform.
+    /// </summary>
+    class Program
+    {
+        const string user = "";
+        const string password = "";
+        const string organization = "";
+        static readonly Uri rootUri = new Uri("{schema}://{authority}");
+        static void Main(string[] args) => MainAsync(args).Wait();
+
+        /// <summary>
+        /// Annotation to the use of the ConfigureAwait method:
+        ///     Because console application by default lack a SynchronizationContext the ConfigureAwait method will have no effect. 
+        ///     Nevertheless one should always specify ConfigureAwait to make the code reusable and to prevent exceptions when adding a library that installs a SynchronizationContext.
+        /// http://stackoverflow.com/questions/25817703/configureawaitfalse-not-needed-in-console-win-service-apps-right
+        /// https://msdn.microsoft.com/en-us/magazine/jj991977.aspx
+        /// </summary>
+        static async Task MainAsync(string[] args)
+        {
+            var client = new PlatformClient(rootUri, user, password, TimeSpan.FromMilliseconds(60000), organization);
+
+            var org = client.Conn.Organizations.First();
+            var fc =
+                await
+                    org.GetFileCabinetsFromFilecabinetsRelationAsync()
+                        .ContinueWith(
+                            t => t.Result.Content
+                                .FileCabinet.First(f => !f.IsBasket))
+                        .ConfigureAwait(false);
+
+            Console.WriteLine($"Selected FileCabinet: {fc.Name}");
+            Console.ReadLine();
+        }
+    }
+}
